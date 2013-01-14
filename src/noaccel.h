7cca3da8034574c75b2287ff154598f45b81a958
Kojo Adams <samanpa@gmail.com>
2013-01-13 20:05:13 -0500

    Add simd intersection

--- src/noaccel.h
+++ src/noaccel.h
@@ -4,8 +4,20 @@
 #include <scene.h>
 
 struct noaccel {
+        
+        noaccel(scene& scene){}
+        
+	void draw(scene& scene, ray& ray, hit& hit) {
+		for (unsigned int p = 0; p < scene._accels.size(); ++p) {
+			ticks start = getticks();
+			scene._accels[p].intersect(p, ray, hit);
+			ticks end = getticks();
+			scene._intersectCost.inc(end - start);
+		}
+	}
 
-	static void draw(scene& scene, ray& ray, hit& hit) {
+
+	void draw(scene& scene, ray4& ray, hit4& hit) {
 		for (unsigned int p = 0; p < scene._accels.size(); ++p) {
 			ticks start = getticks();
 			scene._accels[p].intersect(p, ray, hit);
