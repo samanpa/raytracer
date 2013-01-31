bffbb1b2170e7b4337667b7ddf2a95ac772ecedf
Kojo Adams <samanpa@gmail.com>
2013-01-30 20:44:46 -0500

    Do flush to zero

--- src/banna.cpp
+++ src/banna.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 int main(int argc, char **argv, char **environ) {
+        _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+
 	scene scene;
 	canvas canvas(1024, 1024);
 	camera& camera = scene.getCamera();
